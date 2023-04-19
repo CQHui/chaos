6aa692fbc2e1af2250a800541edd52fe450cf5e7
chenqihui <zjhzcqh@163.com>
2023-04-19 20:11:43 +0800

    use constructor to create instance

---
+++ src/main/java/com/qihui/chaos/reflect/ConstructorDemo.java
@@ -0,0 +1,24 @@
+package com.qihui.chaos.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by Elliott Chen on 2023/4/19 20:06
+ */
+public class ConstructorDemo {
+
+    public int status;
+
+    public ConstructorDemo(int status) {
+        this.status = status;
+    }
+
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<?>[] constructors = ConstructorDemo.class.getConstructors();
+        Constructor<?> constructor = constructors[0];
+        ConstructorDemo obj = (ConstructorDemo) constructor.newInstance(1);
+        System.out.println(obj.status);
+
+    }
+}
